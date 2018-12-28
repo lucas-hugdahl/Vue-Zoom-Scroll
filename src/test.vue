4d713dd55aa582775b8a2ab15d6c4db0d14c9cda
Lucas Hugdahl <Lucas.Hugdahl@indigoslate.com>
2018-12-27 16:00:29 -0800

    first commit

---
+++ src/test.vue
@@ -0,0 +1,28 @@
+
+<script> 
+    import  ZoomScroll  from  "vue-zoom-scroll"
+    export  default {
+        components: {
+            ZoomScroll
+        }
+    }
+</script>
+
+<template>
+    <ZoomScroll
+        initialScale="1"
+        maxScale="5"
+        zoomIntensity="50">
+    
+        <p>You can put any html here like this</p>
+        
+    </ZoomScroll>
+</template>
+
+
+
+
+
+
+
+
